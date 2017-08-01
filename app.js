f3c8f43326aaf5a89ea21d3774ce3bb62c726ebe
Joaquin Ignacio <Keen@Revolt.local>
2017-07-31 17:48:50 -0700

    nearly done just need add review JS

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var basicData = require("./basiccard.js");
 // variables for Inquirer
 var count = 0;
 // number of cards to create
-var cards;
+var cards = 0;
 
 // Stores the array of flash cards
 var cardArray = [];
@@ -25,46 +25,68 @@ function askQuestion () {
 		    choices: ["Basic Card", "Cloze Card"]
 	    }]).then(function(carTypeObj) {
     	// saves the user's requested flash card type to variable. 
-    	cardType = carTypeObj.type;
+    		cardType = carTypeObj.type;
 
-	    if (cardType === "Cloze Card"){
-	        inquirer.prompt([
-	        {
-	            name: "frontCardCloze",
-	            message: "What should the Question be?"
-	        }, {
-	            name: "backCardCloze",
-	            message: "What part do you want to remove? (Must match exactly)"
-	        }]).then(function(cardDataCloze) {
-	            createCloze(cardDataCloze.frontCardCloze, cardDataCloze.backCardCloze);
-	        });
-	    } else {
-        	inquirer.prompt([
-        	{
-	        	name: "frontCardBasic",
-	            message: "What should the Question be?"
-        	}, {
-	            name: "backCardBasic",
-	            message: "What should the answer be?"
-          	}]).then(function(cardDataBasic) {
-              createBasic(cardDataBasic.frontCardBasic, cardDataBasic.backCardBasic);
-        	});
-    	}
+		    if (cardType === "Cloze Card"){
+		        inquirer.prompt([
+		        {
+		            name: "frontCardCloze",
+		            message: "What should the Question be?"
+		        }, {
+		            name: "backCardCloze",
+		            message: "What part do you want to remove? (Must match exactly)"
+		        }]).then(function(cardDataCloze) {
+		            createCloze(cardDataCloze.frontCardCloze, cardDataCloze.backCardCloze);
+		        });
+		    } else {
+	        	inquirer.prompt([
+	        	{
+		        	name: "fullAnswer",
+		            message: "What should the Question be?"
+	        	}, {
+		            name: "cloze",
+		            message: "What should the answer be?"
+	          	}]).then(function(cardDataBasic) {
+	              createBasic(cardDataBasic.fullAnswer, cardDataBasic.cloze);
+	        	});
+    		}
     	});
 	} else {
     	console.log("");
+    	review();
 	}
 };
 
 // function calls to basic card.js to create a basic flash card from that constructor. 
 function createBasic (basicFront, basicBack){
 	if ((basicFront.trim() === "") || (basicBack.trim() === "")) {
-		console.log("You did not enter the correct specifications.  Try again.");
+		console.log("Try again.");
 		askQuestion();
 	} else {
 		cardArray.push(new basicData.BasicCard(basicFront, basicBack));
     	count++;
+    	console.log(cardArray)
 	    askQuestion();
+	}
+};
+
+// function calls to close card.js to create the cloze card from that constructor. 
+function createCloze (fullAnswer, cloze, clozeQuestion) {
+	if ((fullAnswer.trim() === "") || (cloze.trim() === "")) {
+		console.log("Try again.");
+    	askQuestion();
+  	} else if (fullAnswer.trim().includes(cloze.trim())) {
+
+  		// method to remove the "cloze" phrase from the flashcard question.
+    	clozeQuestion = fullAnswer.replace(cloze, "......");
+
+		cardArray.push(new clozeData.ClozeCard(fullAnswer, cloze, clozeQuestion));
+		console.log(cardArray);
+    	count++;
+    	askQuestion();
+    } else {
+    	console.log("Try again.");
+    	askQuestion();
 	}
 };
 
@@ -77,3 +99,35 @@ inquirer.prompt([
     	cards = cardNum.cardNumber;
         askQuestion();
 	});
+
+function review () {
+	console.log("/nTime to review your flash cards!");
+	for (var j=0; j < cardArray.length; j++){
+
+		
+
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ clozecard.js
@@ -0,0 +1,11 @@
+exports.ClozeCard = function(fullAnswer, cloze, clozeQuestion) {
+  this.fullAnswer = fullAnswer;
+  this.cloze = cloze;
+  this.clozeQuestion = clozeQuestion;
+
+
+
+  // this.displayText = this.fullAnswer.replace(this.removedPortion, "...");
+};
+
+
